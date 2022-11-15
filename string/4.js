00045ac778cdc78995aff9a40aa0b3661a0fcd7e
romainlavabre <romainlavabre98@gmail.com>
2022-11-15 23:26:47 +0100

    ADD String/4.js

---
+++ string/4.js
@@ -0,0 +1,20 @@
+// J'apprends à manipuler les chaines de caractères
+
+// Déclarez la variable "result" et stockez y la découpe de "myString" par les espaces
+const myString = "LE DEV EST UN LONG FLEUVE TRANQUILLE";
+// ============================= CODEZ ICI ================================
+
+
+// =========================================================================
+// NE PAS TOUCHER
+try {
+    if (result.length !== 7) {
+        console.log("ERREUR: Ne donnez pas des coups de couteaux au hasard !");
+        return;
+    }
+} catch (e) {
+    console.log("ERREUR: Vous n'avez pas déclaré la variable result");
+    return;
+}
+
+console.log("Félicitation !!! Tu es un excellent boucher");
