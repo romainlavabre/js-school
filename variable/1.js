bebec3c8aa74c9fdafa99885f52a60bf71363a6d
romainlavabre <romainlavabre98@gmail.com>
2022-11-15 16:39:33 +0100

    ADD Variable/1.js

---
+++ variable/1.js
@@ -0,0 +1,51 @@
+// J'apprend à déclarer une variable
+
+// Déclarez et initialisez les variables comme mentionné ci-dessous
+// nommée var1 avec pour type string
+// nommée var2 avec pour type boolean
+// nommée var3 avec pour type integer (nombre entier)
+// nommée var4 avec pour type float (nombre décimal)
+// nommée var5 avec pour type tableau
+// nommée var6 avec pour type objet
+
+// ============================= CODEZ ICI ================================
+
+
+// =========================================================================
+// NE PAS TOUCHER
+if (typeof var1 !== "string") {
+    console.log("ERREUR: La variable var1 n'est pas un string");
+    return;
+}
+
+if (typeof var2 !== "boolean") {
+    console.log("ERREUR: La variable var2 n'est pas un boolean");
+    return;
+}
+
+if (typeof var3 !== "number") {
+    console.log("ERREUR: La variable var3 n'est pas un integer");
+    return;
+}
+
+if (typeof var4 !== "number") {
+    console.log("ERREUR: La variable var4 n'est pas un float");
+    return;
+}
+
+try {
+    if (!Array.isArray(var5)) {
+        console.log("ERREUR: La variable var5 n'est pas un tableau");
+        return;
+    }
+} catch (e) {
+    console.log("ERREUR: La variable var5 n'est pas définie");
+    return;
+}
+
+if (typeof var6 !== "object") {
+    console.log("ERREUR: La variable var6 n'est pas un objet");
+    return;
+}
+
+console.log("Félicitation !!!")
