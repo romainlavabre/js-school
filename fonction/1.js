46e8755bf495ec0ffd3b38eced6a8d6539cb9ebb
romainlavabre <romainlavabre98@gmail.com>
2022-11-15 18:45:53 +0100

    ADD Fonction/1.js

---
+++ fonction/1.js
@@ -0,0 +1,22 @@
+// J'apprends à prendre des décisions de façon conditionnelle
+
+// Creez une fonction nommée "result" qui retourne 256
+
+// ============================= CODEZ ICI ================================
+
+
+// =========================================================================
+// NE PAS TOUCHER
+
+
+try {
+    if (result() !== 256) {
+        console.log("ERREUR: Votre fonction ne retourne pas 256");
+        return;
+    }
+} catch (e) {
+    console.log("ERREUR: Vous n'avez pas déclaré la fonction result");
+    return;
+}
+
+console.log("Félicitation !!!");
