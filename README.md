d2a2975f7a8b54b6c849ef357afd2a33f0508bef
romainlavabre <romainlavabre98@gmail.com>
2022-11-15 17:24:31 +0100

    ADD Tableau/1.js

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ordre d'apprentissage
+
+- Variable
+- Concatenation
+- Condition
+
+!!! Faites les exercices dans l'ordre
---
+++ tableau/1.js
@@ -0,0 +1,20 @@
+// J'apprends les tableaux
+
+// Déclarez la variable "result" en tant que tableau et donnez lui 3 valeurs quelconque
+// ============================= CODEZ ICI ================================
+
+
+// =========================================================================
+// NE PAS TOUCHER
+
+try {
+    if (result.length !== 3) {
+        console.log("ERREUR: Votre tableau doit contenir 3 valeurs");
+        return;
+    }
+} catch (e) {
+    console.log("ERREUR: Vous n'avez pas déclaré la variable result");
+    return;
+}
+
+console.log("Félicitation !!!");
