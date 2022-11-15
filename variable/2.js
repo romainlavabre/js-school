caf9557141b177ef235163eeae4f9870eac1a51a
romainlavabre <romainlavabre98@gmail.com>
2022-11-15 16:44:23 +0100

    ADD Variable/2.js

---
+++ variable/2.js
@@ -0,0 +1,46 @@
+// J'apprend à modifier la valeur d'une variable
+
+// Reprenez les variables de l'exercice 1
+// Sans toucher à leur déclaration initiale, donnez leur une nouvelle valeur
+
+// ============================= CODEZ ICI ================================
+
+
+// =========================================================================
+// NE PAS TOUCHER
+if (typeof var1 !== "string") {
+    console.log("ERREUR: La variable var1 n'est pas un string");
+    return;
+}
+
+if (typeof var2 !== "boolean") {
+    console.log("ERREUR: La variable var2 n'est pas un boolean");
+    return;
+}
+
+if (typeof var3 !== "number") {
+    console.log("ERREUR: La variable var3 n'est pas un integer");
+    return;
+}
+
+if (typeof var4 !== "number") {
+    console.log("ERREUR: La variable var4 n'est pas un float");
+    return;
+}
+
+try {
+    if (!Array.isArray(var5)) {
+        console.log("ERREUR: La variable var5 n'est pas un tableau");
+        return;
+    }
+} catch (e) {
+    console.log("ERREUR: La variable var5 n'est pas définie");
+    return;
+}
+
+if (typeof var6 !== "object") {
+    console.log("ERREUR: La variable var6 n'est pas un objet");
+    return;
+}
+
+console.log("Félicitation !!!")
