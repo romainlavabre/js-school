f1889d796dea99dc6cb15b92d84a60b424970c1a
romainlavabre <romainlavabre98@gmail.com>
2022-11-15 23:22:32 +0100

    ADD String/2.js

---
+++ string/2.js
@@ -0,0 +1,20 @@
+// J'apprends à manipuler les chaines de caractères
+
+// Déclarez la variable "result" et stockez y la valeur de la constante "myString" en majuscule
+const myString = "Le dev est un long fleuve tranquille"
+// ============================= CODEZ ICI ================================
+
+
+// =========================================================================
+// NE PAS TOUCHER
+try {
+    if (result !== "LE DEV EST UN LONG FLEUVE TRANQUILLE") {
+        console.log("ERREUR: La variable result contient des minuscules");
+        return;
+    }
+} catch (e) {
+    console.log("ERREUR: Vous n'avez pas déclaré la variable result");
+    return;
+}
+
+console.log("Félicitation !!!");
