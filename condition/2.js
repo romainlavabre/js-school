6d4e8f46552f6ada64b7f4878b0a0efbf1bc75f2
romainlavabre <romainlavabre98@gmail.com>
2022-11-15 18:00:18 +0100

    ADD Condition/2.js

---
+++ condition/2.js
@@ -0,0 +1,28 @@
+// J'apprends à prendre des décisions de façon conditionnelle
+
+// Stockez dans la variable "result" :
+// true si la taille du tableau est supérieur à 79
+// false sinon
+
+// ATTENTION, faites le de façon conditionnel, ne comptez pas les valeurs du tableau à la main ...
+const myArray = ["string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float", "string", "boolean", "array", "object", "integer", "float"];
+// ============================= CODEZ ICI ================================
+
+
+// =========================================================================
+// NE PAS TOUCHER
+
+
+try {
+    const test = () => {
+    };
+
+    if (result !== (typeof test === "function")) {
+        console.log("ERREUR: Mmmm, retourner bucher vos tableaux !");
+    }
+} catch (e) {
+    console.log("ERREUR: Vous n'avez pas déclaré la variable result");
+    return;
+}
+
+console.log("Félicitation !!!");
