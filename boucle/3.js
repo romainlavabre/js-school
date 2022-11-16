a350c17ba6243ed716ecf76f2cd2f5ea9350769d
romainlavabre <romainlavabre98@gmail.com>
2022-11-16 11:57:18 +0100

    ADD Boucle/3.js

---
+++ boucle/3.js
@@ -0,0 +1,22 @@
+// J'apprends les boucles
+
+// Faites 10 tours de boucle en utilisant la structure "while"
+// À chaque tour de boucle ajouter 1 à la variable "numberOfTurns"
+let numberOfTurns = 0;
+// ============================= CODEZ ICI ================================
+
+
+// =========================================================================
+// NE PAS TOUCHER
+
+if (numberOfTurns === 0) {
+    console.log("ERREUR: Vous n'avez pas fait de tour de boucle");
+    return;
+}
+
+if (numberOfTurns !== 10) {
+    console.log("ERREUR: Vous avez fait " + numberOfTurns + " tours de boucle, pas 10 !");
+    return;
+}
+
+console.log("Félicitation !!!");
